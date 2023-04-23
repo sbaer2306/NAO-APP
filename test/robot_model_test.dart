4d4701c9ffc1d56919a870217c7ffeef426daf69
eugen <eugen.kudraschow@gmail.com>
2023-04-23 17:51:22 +0200

    Bugfix in robot_model_test

--- test/robot_model_test.dart
+++ test/robot_model_test.dart
@@ -4,19 +4,17 @@ import 'package:nao_app/models/robot_model.dart';
 
 void main() {
   group('RobotModel Provider', () {
+    final robotModel = RobotModel(ipAdress: "123");
+
     final provider = RobotProvider();
     test('RobotProvider should start with 0 RobotModels', () {
       expect(provider.items.length, 0);
     });
     test('Add a RobotModel', () {
-      provider.addRobot(RobotModel(ipAdress: '123'));
+      provider.addRobot(robotModel);
       expect(provider.items.length, 1);
     });
     test('Remove a RobotModel', () {
-      final robotModel = RobotModel(ipAdress: "123");
-      provider.addRobot(robotModel);
-      expect(provider.items.length, 1);
-
       provider.removeRobot(robotModel);
       expect(provider.items.length, 0);
     });
