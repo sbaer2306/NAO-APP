3a4d1aaa85c5c6bda13377e0da2131dd53ad1594
Dejan-afk <dejanfra@web.de>
2023-05-11 15:37:14 +0200

    interface with movement  and implemented to model

--- lib/pages/movement_view.dart
+++ lib/pages/movement_view.dart
@@ -1,18 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:nao_app/api/robot_api_interface.dart';
+import '../ui_elements/info_card.dart';
+import '../models/robot_model.dart';
 
-class MovementView extends StatelessWidget {
+import 'dart:developer';
+
+class MovementView extends StatefulWidget {
   const MovementView({Key? key}) : super(key: key);
+
+  @override
+  MovementViewState createState() => MovementViewState();
+}
+
+class MovementViewState extends State<MovementView> {
+  late RobotInterface _robot;
+
+  @override
+  void initState() {
+    super.initState();
+    _robot = RobotModel(ipAddress: '192.168.1.100', name: 'NAO');
+  }
+
+  Future<void> actionMovement(String movement) async {
+    await _robot.setPosture(movement);
+  }
+
+  Future<void> controlMovement(String direction) async {
+    Map<String, dynamic> moveObject = {
+      'enableArmsInWalkAlgorithm': true,
+      'xCoordinate': '0',
+      'yCoordinate': '0',
+      'tCoordinate': '0',
+      'speed': 1.0
+    };
+
+    if (direction == 'Twist Left') moveObject['tCoordinate'] = 1.0;
+    if (direction == 'Forward') moveObject['xCoordinate'] = 1.0;
+    if (direction == 'Twist Right') moveObject['tCoordinate'] = -1.0;
+    if (direction == 'Left') moveObject['yCoordinate'] = 1.0;
+    if (direction == 'Backward') moveObject['xCoordinate'] = -1.0;
+    if (direction == 'Right') moveObject['yCoordinate'] = -1.0;
+
+    await _robot.move(moveObject);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
-      Container(
-        margin: const EdgeInsets.all(20.0),
-        height: 150.0,
-        padding: const EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: Colors.deepPurple[50]),
+      const InfoCard(
+        title: "Lass den NAO bewegen",
+        description:
+            "Wähle aus verschieden vorgefertigten Bewegungen oder steuere ihn nach deinem Belieben.",
       ),
       SizedBox(
           width: double.infinity,
@@ -40,11 +78,31 @@ class MovementView extends StatelessWidget {
         spacing: 8.0,
         alignment: WrapAlignment.center,
         children: [
-          ActionButton(text: "Stand up", function: () => {}),
-          ActionButton(text: "Sit down", function: () {}),
-          ActionButton(text: "Dance", function: () {}),
-          ActionButton(text: "Clap your hands", function: () {}),
-          ActionButton(text: "Yoga", function: () {}),
+          ActionButton(
+              text: "Stand up", function: () => actionMovement("Standing")),
+          ActionButton(
+              text: "Sit down", function: () => actionMovement("Sitting")),
+          ActionButton(
+              text: "Lying Belly",
+              function: () => actionMovement("LyingBelly")),
+          ActionButton(
+              text: "Lying Back", function: () => actionMovement("LyingBack")),
+          ActionButton(
+              text: "Lying Leftside",
+              function: () => actionMovement("LyingLeft")),
+          ActionButton(
+              text: "Lying Rightside",
+              function: () => actionMovement("LyingRight")),
+          ActionButton(text: "Back", function: () => actionMovement("Back")),
+          ActionButton(text: "Left", function: () => actionMovement("Left")),
+          ActionButton(text: "Right", function: () => actionMovement("Right")),
+          ActionButton(
+              text: "Upside Down",
+              function: () => actionMovement("UpsideDown")),
+          ActionButton(
+              text: "Kneeling", function: () => actionMovement("Kneeling")),
+          ActionButton(
+              text: "Lifted", function: () => actionMovement("Lifted")),
         ],
       ),
       SizedBox(
@@ -63,22 +121,25 @@ class MovementView extends StatelessWidget {
           Expanded(
             child: SizedBox(
                 height: 70.0,
-                child:
-                    ControlButton(icon: Icons.u_turn_left, function: () => {})),
+                child: ControlButton(
+                    icon: Icons.u_turn_left,
+                    function: () => controlMovement("Twist left"))),
           ),
           const SizedBox(width: 10.0),
           Expanded(
             child: SizedBox(
                 height: 70.0,
                 child: ControlButton(
-                    icon: Icons.arrow_upward, function: () => {})),
+                    icon: Icons.arrow_upward,
+                    function: () => controlMovement("Forward"))),
           ),
           const SizedBox(width: 10.0),
           Expanded(
             child: SizedBox(
                 height: 70.0,
                 child: ControlButton(
-                    icon: Icons.u_turn_right, function: () => {})),
+                    icon: Icons.u_turn_right,
+                    function: () => controlMovement("Twist right"))),
           ),
           const SizedBox(width: 10.0),
         ],
@@ -89,21 +150,25 @@ class MovementView extends StatelessWidget {
         Expanded(
           child: SizedBox(
               height: 70.0,
-              child: ControlButton(icon: Icons.arrow_back, function: () => {})),
+              child: ControlButton(
+                  icon: Icons.arrow_back,
+                  function: () => controlMovement("Left"))),
         ),
         const SizedBox(width: 10.0),
         Expanded(
           child: SizedBox(
               height: 70.0,
               child: ControlButton(
-                  icon: Icons.arrow_downward, function: () => {})),
+                  icon: Icons.arrow_downward,
+                  function: () => controlMovement("Backward"))),
         ),
         const SizedBox(width: 10.0),
         Expanded(
           child: SizedBox(
               height: 70.0,
-              child:
-                  ControlButton(icon: Icons.arrow_forward, function: () => {})),
+              child: ControlButton(
+                  icon: Icons.arrow_forward,
+                  function: () => controlMovement("Right"))),
         ),
         const SizedBox(width: 10.0),
       ]))
